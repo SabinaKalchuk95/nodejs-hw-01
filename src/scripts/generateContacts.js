122069bdce16d508d7011969f5a168e0eff962cd
Sabina KAlchuk <ssabki229@gmail.com>
2025-09-05 13:51:49 +0200

    one commit

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const generateContacts = async (number) => {
+  const contacts = await readContacts();
+
+  
+  const newContacts = Array.from({ length: number }, () => createFakeContact());
+
+  const updatedContacts = [...contacts, ...newContacts];
+  await writeContacts(updatedContacts);
+
+  console.log(`Generated ${number} new contacts. Total: ${updatedContacts.length}`);
+};
 
 generateContacts(5);
